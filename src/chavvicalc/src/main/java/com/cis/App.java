39e18e7bac5e4a1d9a7355002a8cf9512d82d210
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-08-19 15:29:08 -0700

    Created Shell with Maven

---
+++ src/chavvicalc/src/main/java/com/cis/App.java
@@ -0,0 +1,22 @@
+package com.cis;
+
+/*
+ *  Frank Bernal
+ *  CIS-055 Data Structures
+ *  CIS-059 Object Oriented Programming
+ *  ChavviCalc  
+ *  Start:      19 August 2022
+ *  Finish: 
+ */
+
+// Start App
+public class App {
+    // Start main
+    public static void main( String[] args ) {
+        double a = 0.0;
+        double b = 0.0;
+        // Print menu
+        System.out.println("----------------------------------------");
+        System.out.println("Chavvi Calc");
+    }   // End main
+}   // End App
