a367b007e6ff4b0375c4d3d030c1170ab50f56ef
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-08-19 15:59:38 -0700

    Added Scanner, looped menu until 'q' selected.

--- src/chavvicalc/src/main/java/com/cis/App.java
+++ src/chavvicalc/src/main/java/com/cis/App.java
@@ -9,27 +9,53 @@ package com.cis;
  *  Finish: 
  */
 
+ // Imports
+ import java.util.Scanner;
+
 // Start App
 public class App {
     // Start main
     public static void main( String[] args ) {
-        double a = 0.0;
-        double b = 0.0;
-        // Print menu
-        System.out.println("----------------------------------------");
-        System.out.println("Chavvi Calc");
-        System.out.println("----------------------------------------");
-        System.out.printf("A = %.3f \tB = %.3f\n", a, b);
-        System.out.println("----------------------------------------");
-        System.out.printf("a\tEnter a value for A\n");
-        System.out.printf("b\tEnter a value for B\n");
-        System.out.printf("+\tAdd\n");
-        System.out.printf("-\tSubtract\n");
-        System.out.printf("/\tDivide\n");
-        System.out.printf("*\tMultiply\n");
-        System.out.printf("c\tClear\n");
-        System.out.printf("q\tQuit\n");
-        System.out.println("----------------------------------------");
-        System.out.print("Please enter a selection: ");
+        double a = 0.0;         // Value for A
+        double b = 0.0;         // Value for B
+        String userSelection = "";
+        char menuOption = ' ';
+        boolean quit = false;   // Flag to end program
+
+        // Creat Scanner
+        Scanner readInput = new Scanner(System.in);
+
+        while (!quit) {
+            // Print menu
+            System.out.println("----------------------------------------");
+            System.out.println("Chavvi Calc");
+            System.out.println("----------------------------------------");
+            System.out.printf("A = %.3f \tB = %.3f\n", a, b);
+            System.out.println("----------------------------------------");
+            System.out.printf("a\tEnter a value for A\n");
+            System.out.printf("b\tEnter a value for B\n");
+            System.out.printf("+\tAdd\n");
+            System.out.printf("-\tSubtract\n");
+            System.out.printf("/\tDivide\n");
+            System.out.printf("*\tMultiply\n");
+            System.out.printf("c\tClear\n");
+            System.out.printf("q\tQuit\n");
+            System.out.println("----------------------------------------");
+            System.out.print("Please enter a selection: ");
+
+            // Read input from user
+            userSelection = readInput.next();
+            // Convert to lower
+            userSelection = userSelection.toLowerCase();
+            // Grab character
+            menuOption = userSelection.charAt(0);
+
+            // Control Statement for menuOption
+            // Quit
+            if (menuOption == 'q') {
+                quit = true;
+            }   // End of quit
+
+        }   // End of while
     }   // End main
 }   // End App
